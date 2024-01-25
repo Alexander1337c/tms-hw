42b57083289ae876a148ce29aac0e4a33b4a7d58
Aleksandr Sheshko <sheshko.shura@mail.ru>
2024-01-25 08:42:46 +0300

    corrected hw

--- hw-11/hw-11-2/oop-2.py
+++ hw-11/hw-11-2/oop-2.py
@@ -3,10 +3,11 @@ class Math:
         pass
 
     def is_number(self, a, b):
-        if isinstance(a, int) and isinstance(b, int):
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
             return True
         else:
-            return 'Не число'
+            raise TypeError('Число может быть типа int или float')
+
     def addition(self, a, b):
         if self.is_number(a, b):
             print(a + b)
@@ -28,4 +29,4 @@ class Math:
 
 
 action = Math()
-action.division(3, 3)
+action.division(4, 3)
--- hw-11/hw-11-5/oop-5.py
+++ hw-11/hw-11-5/oop-5.py
@@ -1,16 +1,12 @@
-class SuperStr:
-    def __init__(self, s):
-        self.s = s
+class SuperStr(str):
 
     def is_repeatance(self, s):
-        res = len(self.s) % len(s)
+        res = len(self) % len(s)
         return f'True' if res == 0 else f'False'
 
     def is_palindrome(self):
-        print(f'Строка {self.s} является палиндромом') if (self.s.lower() ==
-                                                           self.s[::-1].lower() or not self.s) else print(
-            'Не палиндром')
+        return self.lower() == self[::-1].lower()
 
 
 sup = SuperStr('abccba')
-sup.is_palindrome()
+print(sup.is_palindrome())
